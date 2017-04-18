8ed5b002e264cd291ea065f2a2ea642e6b9c533d
Rob Mertens <robmertens1993@gmail.com>
2017-04-18 12:16:42 +0200

    modified:   examples/timer_interrupt_test/timer_interrupt_test.ino 	modified:   timer8.cpp

--- examples/timer_interrupt_test/timer_interrupt_test.ino
+++ examples/timer_interrupt_test/timer_interrupt_test.ino
@@ -1,21 +1,35 @@
+/*******************************************************************************
+ * avr-timer-lib
+ * timer_interrupt_test.ino
+ *
+ * Test file for timer interrupts.
+ * I am using an arduino MEGA ADK (AVR atmega2560).
+ * 
+ * @author: 	Rob Mertens
+ * @date:	18/04/2017
+ * @version:	1.1.1   
+ ******************************************************************************/
+
 #include <avr/io.h>
 #include <settings.h>
 #include <timer8.h>
 
-timer8 t2(2, &TCCR2A, &TCCR2B, &TCNT2, &TIMSK2, &OCR2A, &OCR2B);
-
-// INIT
-void setup()
+//INIT.
+void setup
 {
+	//SERIAL.
 	Serial.begin(9600);
+	
+	//TIMER2.
+	timer8 t2(2, &TCCR2A, &TCCR2B, &TCNT2, &TIMSK2, &OCR2A, &OCR2B);
 	
 	t2.setMode(t_mode::NORMAL);
 	t2.setInterruptMode(t_interrupt::OVF);
-	t2.setPrescaler(64);
+	t2.setPrescaler(64); 				// Available prescalers for T2: 1, 8, 32, 64.
 	t2.reset();
 }
 
-// MAIN PROGRAM
+//MAIN PROGRAM.
 void loop()
 {
 	Serial.println(t2.getOverflowCount());
