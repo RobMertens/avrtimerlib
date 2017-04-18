578222b9fb45c71cb099dea445138bc3b970137a
Rob Mertens <robmertens1993@gmail.com>
2017-04-18 12:18:41 +0200

    modified:   examples/timer_interrupt_test/timer_interrupt_test.ino

--- examples/timer_interrupt_test/timer_interrupt_test.ino
+++ examples/timer_interrupt_test/timer_interrupt_test.ino
@@ -14,15 +14,16 @@
 #include <settings.h>
 #include <timer8.h>
 
+//TIMER2.
+timer8 t2(2, &TCCR2A, &TCCR2B, &TCNT2, &TIMSK2, &OCR2A, &OCR2B);
+
 //INIT.
-void setup
+void setup()
 {
 	//SERIAL.
 	Serial.begin(9600);
 	
-	//TIMER2.
-	timer8 t2(2, &TCCR2A, &TCCR2B, &TCNT2, &TIMSK2, &OCR2A, &OCR2B);
-	
+	//TIMER2 SETTINGS.	
 	t2.setMode(t_mode::NORMAL);
 	t2.setInterruptMode(t_interrupt::OVF);
 	t2.setPrescaler(64); 				// Available prescalers for T2: 1, 8, 32, 64.
