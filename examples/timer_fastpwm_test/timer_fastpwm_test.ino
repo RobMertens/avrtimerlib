9a0450d56d75fddc1cc4124c92335190f8495c67
Rob Mertens <robmertens1993@gmail.com>
2017-11-12 16:15:52 +0100

    modified:   examples/timer_fastpwm_test/timer_fastpwm_test.ino 	modified:   examples/timer_interrupt_test/timer_interrupt_test.ino

--- examples/timer_fastpwm_test/timer_fastpwm_test.ino
+++ examples/timer_fastpwm_test/timer_fastpwm_test.ino
@@ -12,17 +12,22 @@
  * @version:	1.1.1
  ******************************************************************************/
 
+//Include avr headers.
 #include "avr/io.h"
-#include "timer8.h"
-#include "timer16.h"
+
+//Include library headers.
+#include "timer8.hpp"
+#include "timer16.hpp"
 
 //LED INDICATOR.
 #define DDRLED DDRB
 #define OUTLED PORTB
 
+using namespace avr;
+
 //TIMER0.
-timer8	t0(t_settings::alias::T0);
-timer16	t1(t_settings::alias::T1);
+Timer8	t0(t_alias::T0);
+Timer16	t1(t_alias::T1);
 
 //INIT.
 void setup()
@@ -32,23 +37,19 @@ void setup()
   OUTLED &= 0x7F;								// Initially turn LED off.
 
 	//TIMER0 SETTINGS.
-	t0.initialize(t_settings::mode::PWM_F,
-								t_settings::channel::A,
-								t_settings::inverted::NORMAL);		// t_settings::channel::A sets pwm channels A on T0 (Pin PB7).
-										// I have a LED connected to PB7.
-	t0.setPrescaler(1024);							// Available prescalers for T0: 1, 8, 64, 256, 1024.
-	t0.setDutyCycleA(0.0);							// Set the dutycycle (OFF=0.0, FULL=1.0).
-	t0.reset();								// Set count value to zero.
+	t0.initialize(t_mode::PWM_F, t_channel::A, t_inverted::NORMAL);		// t_channel::A sets pwm channels A on T0 (Pin PB7).
+																																		// I have a LED connected to PB7.
+	t0.setPrescaler(1024);																						// Available prescalers for T0: 1, 8, 64, 256, 1024.
+	t0.setDutyCycleA(0.0);																						// Set the dutycycle (OFF=0.0, FULL=1.0).
+	t0.reset();																												// Set count value to zero.
 
 	//TIMER1 SETTINGS.
-	t1.initialize(t_settings::mode::PWM_F,
-								t_settings::channel::BC_TOP,
-								t_settings::inverted::NORMAL);	// t_settings::channel::BC_TOP sets A as TOP and pwm channels B and C.
-	t1.setCompareValueA(0xC8);						// REMARK: If you specify the TOP, you lose resolution.
-	t1.setPrescaler(1024);							// Available prescalers for T0: 1, 8, 64, 256, 1024.
-	t1.setDutyCycleB(0.45);							// Set the dutycycle (OFF=0.0, FULL=1.0).
-	t1.setDutyCycleC(0.78);							// Set the dutycycle.
-	t1.reset();								// Set count value to zero.
+	t1.initialize(t_mode::PWM_F, t_channel::BC_TOP, t_inverted::NORMAL);	// t_channel::BC_TOP sets A as TOP and pwm channels B and C.
+	t1.setCompareValueA(0xC8);																						// REMARK: If you specify the TOP, you lose resolution.
+	t1.setPrescaler(1024);																								// Available prescalers for T0: 1, 8, 64, 256, 1024.
+	t1.setDutyCycleB(0.45);																								// Set the dutycycle (OFF=0.0, FULL=1.0).
+	t1.setDutyCycleC(0.78);																								// Set the dutycycle.
+	t1.reset();																														// Set count value to zero.
 }
 
 //MAIN PROGRAM.
